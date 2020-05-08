5b390318d6e3331bf0f0fe8757edfaa0624e694d
Sergey <sssssergo15@gmail.com>
2020-05-08 19:33:10 +0600

    add migrations

---
+++ database/migrations/2020_05_08_131354_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image')->nullable(0);
+            $table->text('content');
+            $table->integer('user_id')->nullable();
+            $table->integer('category')->nullable();
+            $table->integer('views')->default(0);
+            $table->integer('status')->default(0);
+            $table->integer('is_featured')->default(0);
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
